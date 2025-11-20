614ad75c367b427b9c78309fe496501519748517
Youssef Ennajar <yousenna@e1r10p8.1337.ma>
2025-11-20 18:28:03 +0100

    correct normennet

--- header.h
+++ header.h
@@ -1,18 +1,18 @@
 #ifndef HEADER_H
-#define HEADER_H
+# define HEADER_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-int ft_printf(const char *, ...);
-void write_char(int c, int *len);
-void ft_putnbr(int n, int *len);
-void write_str(const char *s, int *len);
-void ft_putnbr_unsigned(unsigned int num, int *len);
-void printhex(va_list arg, int *len);
-void print_x(va_list arg, int *len);
-void print_X(va_list arg, int *len);
+# include <limits.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+int		ft_printf(const char *, ...);
+void	write_char(int c, int *len);
+void	ft_putnbr(int n, int *len);
+void	write_str(const char *s, int *len);
+void	ft_putnbr_unsigned(unsigned int num, int *len);
+void	printhex(va_list arg, int *len);
+void	print_x(va_list arg, int *len);
+void	print_X(va_list arg, int *len);
 
 #endif
